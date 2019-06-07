ce58a47218eec244442fbbfbe6ace6ed80de75d7
hehnope <24252625+hehnope@users.noreply.github.com>
2019-06-07 13:42:00 -0700

    initial

--- dcos/resource_dcos_job.go
+++ dcos/resource_dcos_job.go
@@ -1,5 +1,146 @@
 package dcos
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dcos/client-go/dcos"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceDcosSecret() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceDcosJobCreate,
+		Read:   resourceDcosJobRead,
+		Update: resourceDcosJobUpdate,
+		Delete: resourceDcosJobDelete,
+		// Importer: &schema.ResourceImporter{
+		// 	State: schema.ImportStatePassthrough,
+		// },
+
+		SchemaVersion: 1,
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(20 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"path": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"value": {
+				Type:      schema.TypeString,
+				Required:  true,
+				ForceNew:  false,
+				Sensitive: true,
+			},
+			"store": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Default:  "default",
+			},
+		},
+	}
+}
+
+func resourceDcosJobCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*dcos.APIClient)
+	ctx := context.TODO()
+
+	secretsV1Secret := dcos.SecretsV1Secret{}
+	secretsV1Secret.Value = d.Get("value").(string)
+
+	pathToSecret := d.Get("path").(string)
+
+	store := d.Get("store").(string)
+
+	resp, err := client.Secrets.CreateSecret(ctx, store, encodePath(pathToSecret), secretsV1Secret)
+
+	log.Printf("[TRACE] Create %s, %s - %v", store, pathToSecret, resp)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(generateID(store, pathToSecret))
+	return nil
+}
+
+func resourceDcosJobRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*dcos.APIClient)
+	ctx := context.TODO()
+
+	store := d.Get("store").(string)
+	pathToSecret := d.Get("path").(string)
+
+	secret, resp, err := client.Secrets.GetSecret(ctx, store, encodePath(pathToSecret), nil)
+
+	log.Printf("[TRACE] Read - %v", resp)
+
+	if resp.StatusCode == http.StatusNotFound {
+		log.Printf("[INFO] Read - %s not found", pathToSecret)
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return nil
+	}
+
+	d.Set("value", secret.Value)
+	d.SetId(generateID(store, pathToSecret))
+
+	return nil
+}
+
+func resourceDcosJobUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*dcos.APIClient)
+	ctx := context.TODO()
+
+	secretsV1Secret := dcos.SecretsV1Secret{}
+	secretsV1Secret.Value = d.Get("value").(string)
+
+	pathToSecret := d.Get("path").(string)
+
+	store := d.Get("store").(string)
+
+	_, err := client.Secrets.UpdateSecret(ctx, store, encodePath(pathToSecret), secretsV1Secret)
+
+	if err != nil {
+		return err
+	}
+
+	return resourceDcosSecretRead(d, meta)
+}
+
+func resourceDcosJobDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*dcos.APIClient)
+	ctx := context.TODO()
+
+	pathToSecret := d.Get("path").(string)
+	store := d.Get("store").(string)
+
+	resp, err := client.Secrets.DeleteSecret(ctx, store, pathToSecret)
+
+	if resp.StatusCode == http.StatusNotFound {
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+	return nil
+}
+
 //
 // import (
 // 	"time"
