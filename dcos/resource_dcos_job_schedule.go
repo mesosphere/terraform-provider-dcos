35d6ae36d858f95d54245568421ca862afd02957
hehnope <24252625+hehnope@users.noreply.github.com>
2019-06-17 09:54:50 -0700

    add schema for dcos_job_schedule

--- dcos/resource_dcos_job_schedule.go
+++ dcos/resource_dcos_job_schedule.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/dcos/client-go/dcos"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
 )
 
 func resourceDcosJobSchedule() *schema.Resource {
@@ -31,6 +32,39 @@ func resourceDcosJobSchedule() *schema.Resource {
 				Required:    true,
 				ForceNew:    true,
 				Description: "Unique identifier for the job.",
+			},
+			"cron": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    false,
+				Description: "Cron based schedule string",
+			},
+			"concurrency_policy": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     false,
+				Description:  "Defines the behavior if a job is started, before the current job has finished. ALLOW will launch a new job, even if there is an existing run.",
+				ValidateFunc: validation.StringInSlice([]string{"ALLOW"}, false),
+			},
+			"enabled": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				ForceNew:    false,
+				Description: "Defines if the schedule is enabled or not.",
+			},
+			"starting_deadline_seconds": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ForceNew:     false,
+				Description:  "The number of seconds until the job is still considered valid to start.",
+				ValidateFunc: validation.IntAtLeast(32),
+			},
+			"timezone": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     false,
+				Description:  "IANA based time zone string. See http://www.iana.org/time-zones for a list of available time zones.",
+				ValidateFunc: validateRegexp("^[a-zA-Z]+/?[a-zA-Z]+$"),
 			},
 		},
 	}
