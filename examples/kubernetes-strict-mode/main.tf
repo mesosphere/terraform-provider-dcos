873d15956d763e4130dc961934770a0ca1b1ceb0
Jan Ulferts <julferts@mesosphere.com>
2019-07-11 16:21:47 +0200

    add kubernetes strictmode example

---
+++ examples/kubernetes-strict-mode/main.tf
@@ -0,0 +1,58 @@
+provider "dcos" {}
+
+variable "app_id" {
+  default = "kubernetes"
+}
+
+resource "tls_private_key" "service_account_key" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "dcos_iam_service_account" "service_account" {
+  uid         = "${var.app_id}-principal"
+  description = "Kubernets Service Account"
+  public_key  = "${tls_private_key.service_account_key.public_key_pem}"
+}
+
+locals {
+  principal_create_grants = [
+    "dcos:mesos:master:reservation:role:kubernetes-role",
+    "dcos:mesos:master:framework:role:kubernetes-role",
+    "dcos:mesos:master:task:user:nobody",
+  ]
+}
+
+resource "dcos_iam_grant_user" "principal_create_grants" {
+  count    = "${length(local.principal_create_grants)}"
+  uid      = "${dcos_iam_service_account.service_account.uid}"
+  resource = "${element(local.principal_create_grants, count.index)}"
+  action   = "create"
+}
+
+# could be predefined data resource or special "service_account_secret"
+locals {
+  jenkins_secret = {
+    scheme         = "RS256"
+    uid            = "${dcos_iam_service_account.service_account.uid}"
+    private_key    = "${tls_private_key.service_account_key.private_key_pem}"
+    login_endpoint = "https://master.mesos/acs/api/v1/auth/login"
+  }
+}
+
+resource "dcos_secret" "secret" {
+  path = "${var.app_id}/sa"
+
+  value = "${jsonencode(local.jenkins_secret)}"
+}
+
+resource "dcos_package" "package" {
+  name   = "kubernetes"
+  app_id = "${var.app_id}"
+
+  config_json = <<EOF
+{"service":{"service_account": "${dcos_iam_service_account.service_account.uid}", "service_account_secret": "${dcos_secret.secret.path}"}}
+EOF
+
+  depends_on = ["dcos_iam_grant_user.principal_create_grants"]
+}
