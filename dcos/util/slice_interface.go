5a8d7ff5a385d4c8020c6f8965d3dd6df059ae50
Jan Ulferts <julferts@mesosphere.com>
2019-10-11 17:20:46 +0200

    add helper for dealing with interface slices

---
+++ dcos/util/slice_interface.go
@@ -0,0 +1,31 @@
+package util
+
+// InterfaceSliceInt32 transforms a []interface{} into []int32
+func InterfaceSliceInt32(s []interface{}) (r []int32, ok bool) {
+	ok = false
+	r = make([]int32, 0)
+	for _, v := range s {
+		if val, k := v.(int32); k {
+			// we found at least one value
+			ok = true
+			r = append(r, val)
+		}
+	}
+
+	return r, ok
+}
+
+// InterfaceSliceString transforms a []interface{} into []string
+func InterfaceSliceString(s []interface{}) (r []string, ok bool) {
+	ok = false
+	r = make([]string, 0)
+	for _, v := range s {
+		if val, k := v.(string); k {
+			// we found at least one value
+			ok = true
+			r = append(r, val)
+		}
+	}
+
+	return r, ok
+}
