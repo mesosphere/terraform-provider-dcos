8b835a5d6cdc0e13dde57507521181c00609b0f7
Jan Ulferts <julferts@mesosphere.com>
2019-10-24 16:21:23 +0200

    include secrets into example

--- examples/marathon/main.tf
+++ examples/marathon/main.tf
@@ -1,20 +1,35 @@
 provider "dcos" {}
 
+resource "dcos_security_secret" "env" {
+  path  = "marathon-env-sercret"
+  value = "pong"
+}
+
+resource "dcos_security_secret" "file" {
+  path  = "marathon-file-sercret"
+  value = "file-pong"
+}
+
 resource "dcos_marathon_app" "marathon-ping" {
-  app_id    = "/marathon-ping"
+  app_id    = "/marathon-test-ping"
   cpus      = 0.1
   mem       = 32
   instances = 1
 
   cmd = <<EOF
-echo "pong" > index.html && python -m http.server $PORT0
+  cd $${SANDBOX_PATH};cat index2.html; echo $${MARATHON_ENV_SECRET} > index.html && python -m http.server $PORT0
 EOF
 
   container {
-    type = "DOCKER"
+    type = "MESOS"
 
     docker {
       image = "python:3"
+    }
+
+    volumes {
+      container_path = "index2.html"
+      secret         = "secret2"
     }
   }
 
@@ -26,6 +41,11 @@ EOF
     interval_seconds         = 10
     timeout_seconds          = 10
     max_consecutive_failures = 3
+  }
+
+  secrets {
+    "MARATHON_ENV_SECRET" = "${dcos_security_secret.env.path}"
+    "secret2"             = "${dcos_security_secret.file.path}"
   }
 
   port_definitions {
